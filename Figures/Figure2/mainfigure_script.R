ae05990b9b4607809d07dac5546f13ee1f7f5396
Siyuan <415110769@qq.com>
2025-01-17 15:26:48 -0600

    Update mainfigure_script.R

--- Figures/Figure2/mainfigure_script.R
+++ Figures/Figure2/mainfigure_script.R
@@ -1,31 +1,22 @@
 
-myeloid_all_filtered <- subset(all_cell, celltype == "myeloid")
+fibro_all_diet <- readRDS("")
 
-###--------Figure 2a--------###
-DimPlot(myeloid_all_filtered, label = T, label.size = 3.5, cols = mye_col, group.by = "cellstate")
+# fig2a fibro UMAP-------------------------------------------------------------------
+DimPlot(fibro_all_diet, raster = F, cols = fibro_col, label = F)
 
-###--------Figure 2c--------###
-macrophage_all <- subset(myeloid_all_filtered, cellstate %in% c('Macro_C3', 'Macro_LYVE1', 'Macro_CCL4',  'Macro_APOC1', 'Macro_ISG15'))
-VlnPlot(macrophage_all, c('APOC1','SPP1','APOE', 'LIPA' ,'FABP5', 'LPL', 'TREM2'), pt.size = 0, cols = mye_col[c(3:7)], stack = T, flip = T, fill.by = 'ident') & NoLegend()
-VlnPlot(macrophage_all, c('FOLR2', 'SLC40A1', 'SELENOP', 'LYVE1', 'MRC1', 'CD163', 'MAF'), pt.size = 0, cols = mye_col[c(3:7)], stack = T, flip = T, fill.by = 'ident') & NoLegend()
 
-###--------Figure 2d--------###
-macrophage_signature <- myeloid_all_filtered_marker %>% filter(cluster %in% c('Macro_C3', 'Macro_LYVE1', 'Macro_CCL4',  'Macro_APOC1', 'Macro_ISG15')) %>% group_by(cluster) %>% filter(avg_log2FC > 0.5 & pct.2 < 0.7 & p_val_adj < 0.05) %>% top_n(n = 50, wt = avg_log2FC)
+# fig2c fibro markers-------------------------------------------------------------------
+VlnPlot(fibro_all_diet, c('COL15A1', 'IGF1', 'DLK1', 'IGFBP2', 'ACTA2', 'FAP', 'TAGLN', 'POSTN'), pt.size = 0, cols = fibro_col, stack = T, flip = T, fill.by = 'ident') & NoLegend()
 
-macrophage_df <- macrophage_signature[,6:7]
-macrophage_dfsample <- split(macrophage_df$gene, macrophage_df$cluster)
-{
-  macrophage0_gene <- bitr(as.character(macrophage_dfsample$Macro_C3), fromType = "SYMBOL", toType = "ENTREZID", OrgDb = "org.Hs.eg.db")
-  macrophage1_gene <- bitr(as.character(macrophage_dfsample$Macro_LYVE1), fromType = "SYMBOL", toType = "ENTREZID", OrgDb = "org.Hs.eg.db")
-  macrophage2_gene <- bitr(as.character(macrophage_dfsample$Macro_CCL4), fromType = "SYMBOL", toType = "ENTREZID", OrgDb = "org.Hs.eg.db")
-  macrophage3_gene <- bitr(as.character(macrophage_dfsample$Macro_APOC1), fromType = "SYMBOL", toType = "ENTREZID", OrgDb = "org.Hs.eg.db")
-  macrophage4_gene <- bitr(as.character(macrophage_dfsample$Macro_ISG15), fromType = "SYMBOL", toType = "ENTREZID", OrgDb = "org.Hs.eg.db")
-}
-macrophageph_genes <- list(C3 = macrophage0_gene$ENTREZID, LYVE1 = macrophage1_gene$ENTREZID, CCL4 = macrophage2_gene$ENTREZID, APOC1 = macrophage3_gene$ENTREZID, ISG15 = macrophage4_gene$ENTREZID)
-macrophageph_GOclusterplot <- compareCluster(geneCluster = macrophageph_genes, fun = "enrichGO", OrgDb = "org.Hs.eg.db", ont = "BP")
-dotplot(macrophageph_GOclusterplot, showCategory = 5) + scale_y_discrete(labels=function(x) str_wrap(x, width=85)) + 
-  scale_colour_gradientn(colours = (paletteer::paletteer_c("grDevices::Blue-Red 2", 50)))
 
-macrophageph_GOclusterplot1 <- simplify(macrophageph_GOclusterplot, cutoff=0.5, by="p.adjust", select_fun=min)
-dotplot(macrophageph_GOclusterplot1, showCategory = 5) + scale_y_discrete(labels=function(x) str_wrap(x, width=85)) + 
-  scale_colour_gradientn(colours = (paletteer::paletteer_c("grDevices::Blue-Red 2", 50)))
+
+fibro_mmp_marker <- fibro_all_marker %>% filter(grepl("^MMP", gene)) %>% pull(gene)
+VlnPlot(fibro_all_diet, fibro_mmp_marker, pt.size = 0, cols = fibro_col, stack = T, flip = T, fill.by = 'ident') & NoLegend()
+DotPlot(fibro_all_diet, features = unique(fibro_mmp_marker)) + RotatedAxis() + scale_color_gradientn(colours = viridis::viridis(20)) & coord_flip()
+
+fibro_all_diet <- AddMetaData(fibro_all_diet, metadata = hbca_dcis_ibc.integrated.b1.b2.clean.cellanno.met.tis.t)
+VlnPlot(fibro_all_diet, 'TGFB1', split.by = 'tissue_type', pt.size = 0)
+ 
+
+# fig4h-vlnplot -----------------------------------------------------------
+VlnPlot(vascular_all_diet, c('APLNR', 'INSR', 'FLT1', 'KDR', 'NRP1', 'VWA1', 'COL4A1', 'COL4A2', 'COL6A2','LAMA4','LAMB1'), pt.size = 0, cols = vas_col, stack = T, flip = T, fill.by = 'ident') & NoLegend()
