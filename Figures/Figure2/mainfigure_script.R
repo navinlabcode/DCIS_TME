e3265b1dc2206b633dd5f6ae13d501b92dd72561
Siyuan <415110769@qq.com>
2025-01-17 15:30:44 -0600

    Update mainfigure_script.R

--- Figures/Figure2/mainfigure_script.R
+++ Figures/Figure2/mainfigure_script.R
@@ -8,15 +8,13 @@ DimPlot(fibro_all_diet, raster = F, cols = fibro_col, label = F)
 # fig2c fibro markers-------------------------------------------------------------------
 VlnPlot(fibro_all_diet, c('COL15A1', 'IGF1', 'DLK1', 'IGFBP2', 'ACTA2', 'FAP', 'TAGLN', 'POSTN'), pt.size = 0, cols = fibro_col, stack = T, flip = T, fill.by = 'ident') & NoLegend()
 
-
-
 fibro_mmp_marker <- fibro_all_marker %>% filter(grepl("^MMP", gene)) %>% pull(gene)
 VlnPlot(fibro_all_diet, fibro_mmp_marker, pt.size = 0, cols = fibro_col, stack = T, flip = T, fill.by = 'ident') & NoLegend()
 DotPlot(fibro_all_diet, features = unique(fibro_mmp_marker)) + RotatedAxis() + scale_color_gradientn(colours = viridis::viridis(20)) & coord_flip()
 
-fibro_all_diet <- AddMetaData(fibro_all_diet, metadata = hbca_dcis_ibc.integrated.b1.b2.clean.cellanno.met.tis.t)
-VlnPlot(fibro_all_diet, 'TGFB1', split.by = 'tissue_type', pt.size = 0)
  
 
-# fig4h-vlnplot -----------------------------------------------------------
+vascular_all_diet <- readRDS("")
+
+# fig2i vasuclar markers -----------------------------------------------------------
 VlnPlot(vascular_all_diet, c('APLNR', 'INSR', 'FLT1', 'KDR', 'NRP1', 'VWA1', 'COL4A1', 'COL4A2', 'COL6A2','LAMA4','LAMB1'), pt.size = 0, cols = vas_col, stack = T, flip = T, fill.by = 'ident') & NoLegend()
