0f026243cc67dc085ff6f168a9b8f418d09ccd70
Siyuan <415110769@qq.com>
2025-07-11 17:35:34 -0500

    Update mainfigure_script.R

--- Figures/Figure2/mainfigure_script.R
+++ Figures/Figure2/mainfigure_script.R
@@ -19,6 +19,13 @@ fibro_all_marker <- FindAllMarkers(fibro_all, logfc.threshold = 0.3, only.pos = 
 
 DimPlot(fibro_all)
 
+# fig2b fibroblast cell state comparison---------------------------------------------
+supp_pwc <- cells.pt.final_sum3_sel %>% filter(!cells_State %in% cell_stat_plot) %>% group_by(cells_State) %>% rstatix::dunn_test(Count ~ tissue_upd_0924, p.adjust.method = "BH") %>% rstatix::add_xy_position(x = "tissue_upd_0924")
+
+ggplot(cells.pt.final_sum3_sel %>% filter(!cells_State %in% cell_stat_plot), aes(x = tissue_upd_0924, y = Count)) + geom_boxplot(outliers = F) + geom_jitter(aes(colour = tissue_upd_0924), size = 1) +
+  facet_wrap(facets = vars(cells_State), nrow = 1) + theme_bw()+rremove("x.ticks") + rremove("x.text") + scale_color_manual(values = tissue_type_col) + 
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) + stat_pvalue_manual(supp_pwc, hide.ns = TRUE, label = "p = {scales::pvalue(p.adj)}")
+
 
 # fig2c fibro markers-------------------------------------------------------------------
 VlnPlot(fibro_all_diet, c('COL15A1', 'IGF1', 'DLK1', 'IGFBP2', 'ACTA2', 'FAP', 'TAGLN', 'POSTN'), pt.size = 0, cols = fibro_col, stack = T, flip = T, fill.by = 'ident') & NoLegend()
@@ -27,9 +34,28 @@ fibro_mmp_marker <- fibro_all_marker %>% filter(grepl("^MMP", gene)) %>% pull(ge
 VlnPlot(fibro_all_diet, fibro_mmp_marker, pt.size = 0, cols = fibro_col, stack = T, flip = T, fill.by = 'ident') & NoLegend()
 DotPlot(fibro_all_diet, features = unique(fibro_mmp_marker)) + RotatedAxis() + scale_color_gradientn(colours = viridis::viridis(20)) & coord_flip()
 
- 
 
-vascular_all_diet <- readRDS("")
 
-# fig2i vasuclar markers -----------------------------------------------------------
-VlnPlot(vascular_all_diet, c('APLNR', 'INSR', 'FLT1', 'KDR', 'NRP1', 'VWA1', 'COL4A1', 'COL4A2', 'COL6A2','LAMA4','LAMB1'), pt.size = 0, cols = vas_col, stack = T, flip = T, fill.by = 'ident') & NoLegend()
+
+
+# vascular analysis -----------------------------------------------------------
+# fig2g vascular state umap-------------------------------------------------------------------
+vas_all_unfiltered <- readRDS("vas.seurat.inte.proj.all.upd.rds")
+DefaultAssay(vas_all_unfiltered) <- "integrated"
+
+vas_all <- subset(vas_all_unfiltered, nFeature_RNA > 500)
+DefaultAssay(vas_all) <- "integrated"
+vas_all <- ScaleData(vas_all, verbose = FALSE)
+vas_all <- RunPCA(vas_all, npcs = 30, verbose = FALSE)
+vas_all <- RunUMAP(vas_all, reduction = "pca", dims = 1:20)
+vas_all <- FindNeighbors(vas_all, dims = 1:20)
+vas_all <- FindClusters(vas_all, resolution = 0.2)
+                                                                      
+DefaultAssay(vas_all) <- "RNA"
+vas_all_marker <- FindAllMarkers(vas_all, logfc.threshold = 0.3, only.pos = T, slot = "data") #for cluster number iden
+
+DimPlot(vas_all)
+
+# fig2i vascular state umap-------------------------------------------------------------------
+VlnPlot(vas_all, c('APLNR', 'INSR', 'FLT1', 'KDR', 'NRP1', 'VWA1', 'COL4A1', 'COL4A2', 'COL6A2','LAMA4','LAMB1'), 
+        pt.size = 0, cols = vas_col, stack = T, flip = T, fill.by = 'ident') & NoLegend()
