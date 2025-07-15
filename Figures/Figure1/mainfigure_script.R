41d9e2b788a637bc3c2ea07c42192535d7646f1c
Siyuan He <She2@mdanderson.org>
2025-07-15 14:41:23 -0500

    update figure 1

--- Figures/Figure1/mainfigure_script.R
+++ Figures/Figure1/mainfigure_script.R
@@ -1,17 +1,22 @@
-## Note: the analysis below is based on the integrated object ##
+library(Seurat)
+library(dplyr)
+library(ggplot2)
+
+## Note: the analyses below are based on the integrated object ##
 
 all_cell <- readRDS("~/all_cell.rds")
 
-###--------Figure 1b--------###
+###--------Figure 1c--------###
 DimPlot(all_cell, group.by = "celltype", cols = celltype_col, label = T, raster = F)
 
-###--------Figure 1c--------###
+###--------Figure 1d--------###
 Idents(all_cell) <- all_cell$celltype
 DefaultAssay(all_cell) <- "RNA"
 hbca_dcis_ibc.combined_marker <- FindAllMarkers(all_cell, logfc.threshold = 0.3, only.pos = T, slot = "data")
 
-normal_dcis_idc_deg_top8 <- normal_dcis_idc_deg %>% filter(p_val_adj < 0.05) %>% group_by(cluster) %>% filter(pct.1 > 0.5) %>% filter(pct.2 < 0.5) %>% top_n(n=7, wt=avg_log2FC) #%>% filter(pct.1 > 0.8)
+normal_dcis_idc_deg_top8 <- hbca_dcis_ibc.combined_marker %>% filter(p_val_adj < 0.05) %>% group_by(cluster) %>% filter(pct.1 > 0.5) %>% filter(pct.2 < 0.5) %>% top_n(n=7, wt=avg_log2FC) #%>% filter(pct.1 > 0.8)
 normal_dcis_idc_deg_top8 <- normal_dcis_idc_deg_top8[with(normal_dcis_idc_deg_top8, order(ave(seq_along(cluster), cluster,  FUN = function(x) match(cluster[x], c("Basal", "lumhr", "lumsec",  "Bcell", "NK_T", "Myeloid", "Fibroblast", "Vascular", "Lymphatic", "PeriVas"))))), ]
+
 DotPlot(object = all_cell, group.by = 'celltype', features = unique(normal_dcis_idc_deg_top8$gene))+ RotatedAxis() + scale_color_gradientn(colours = rev(paletteer_c("ggthemes::Red-Blue Diverging", 100)))
 
 ###--------Figure 1d--------###
@@ -26,9 +31,17 @@ all_cell$tissue_type_mer <- factor(all_cell$tissue_type_mer, levels = c("Normal"
 
 DimPlot(all_cell, group.by = 'tissue_type_mer', cols = tissue_type_col, raster = F, shuffle = T)
                                                                                               
-###--------Figure 1f--------### 
+###--------Figure 1f--------###
+all_cell$celltype_com <- all_cell$celltype_upd1
+all_cell$celltype_com <- factor(all_cell$celltype_com, 
+                                levels = c("Basal", "lumhr", "lumsec",  "Bcell", "NK_T", "Myeloid", "Fibroblast", "Vascular", "Lymphatic", "PeriVas"))
+
+all_cell$tumor_normal <- ifelse(rownames(all_cell@meta.data) %in% rownames(tumor_obj_rm_meta_data), 
+                                'aneuploid', 'diploid')
+
 hbca_dcis_ibc.integrated.all.meta <- data.frame(sample = all_cell$orig.ident, proj_ori = all_cell$project,
                                                 celltype = all_cell$celltype_com, ploidy = all_cell$tumor_normal)
+
 hbca_dcis_ibc.integrated.all.meta.tis <- merge(hbca_dcis_ibc.integrated.all.meta, dcis_clinical, by = 'sample', all.x = T)
 all_tme_cell_count2_cli_upd <- data.frame(table(hbca_dcis_ibc.integrated.all.meta.tis$tissue_upd_0924, hbca_dcis_ibc.integrated.all.meta.tis$celltype))
 allcells_kept_sum1_proj_upd_mel_sel <- all_tme_cell_count2_cli_upd %>% filter(Var1 %in% c("Normal", "DCIS_yes", "synch_yes", "IDC_yes"))
@@ -39,6 +52,8 @@ ggplot(allcells_kept_sum1_proj_upd_mel_sel %>% filter(!Var2 %in% c('lumhr', 'lum
   scale_x_discrete(limits = c("Normal", "DCIS_yes", "synch_yes", "IDC_yes"))+theme(axis.text.x=element_text(angle=50,hjust=1,vjust=1))
                                                                                               
 ###--------Figure 1g--------### 
+dcis_clinical <- read.csv('./dcis_project_tissuetype_0916.csv', header = T)
+
 all_tme_sample_cell_count <- data.frame(table(hbca_dcis_ibc.integrated.all.meta$sample, hbca_dcis_ibc.integrated.all.meta$celltype))
 all_tme_sample_cell_count_tis <- merge(all_tme_sample_cell_count, dcis_clinical, by.x = 'Var1', by.y = 'sample')
 all_tme_sample_cell_count_tis_sel <- all_tme_sample_cell_count_tis %>% filter(tissue_upd_0924 %in% c("Normal", "DCIS_yes", "synch_yes", "IDC_yes"))
@@ -47,6 +62,7 @@ all_tme_sample_cell_count_tis_sel_prop$tissue_upd_0924 <- factor(all_tme_sample_
 
 pwc_celltype <- all_tme_sample_cell_count_tis_sel_prop %>% filter(!Var2 %in% c('Vascular',  'Lymphatic', 'Bcell', 'PeriVas')) %>% group_by(Var2) %>% 
   rstatix::dunn_test(Prop ~ tissue_upd_0924, p.adjust.method = "BH") %>% rstatix::add_xy_position(x = "tissue_upd_0924")
+
 ggplot(all_tme_sample_cell_count_tis_sel_prop %>% filter(!Var2 %in% c('Vascular', 'Lymphatic', 'Bcell', 'PeriVas')), aes(x = tissue_upd_0924, y = Prop)) + geom_boxplot(outliers = F) + geom_jitter(aes(colour = tissue_upd_0924), size = 0.9) +
   facet_wrap(facets = vars(Var2), nrow = 1) + theme_bw()+rremove("x.ticks") + rremove("x.text") + scale_color_manual(values = tissue_type_col) + 
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) + stat_pvalue_manual(pwc_celltype, hide.ns = TRUE, label = "p = {scales::pvalue(p.adj)}")
--- Figures/Figure1/supplementary_script.R
+++ Figures/Figure1/supplementary_script.R
@@ -1,54 +1,118 @@
 library(Seurat)
+library(dplyr)
+library(infercna)
+library(ggpubr)
+library(ComplexHeatmap)
+library(circlize)
+library(cowplot)
 
 
 ###--------Supplementary figure 1a--------###
+pdf('./all_tumor_cnv_heatmap.pdf', width=12, height=18.5)
 
+sample_cell_df <- read.csv('./all_tumor_sample_cell.csv', header = T)
+DCIS_copykat_cnv_sel <- read.csv('./DCIS_copykat_cnv_sel_all.csv', header = T, row.names = 1)
+sample_record <- read.csv('./sample_record.csv', header = T, row.names = 1)
+DCIS_copykat_cnv <- read.table("./BCMDCIS01T_copykat_CNA_results.txt", header = T)
 
+DCIS_copykat_cnv_sel_all <- cbind(sample_record, DCIS_copykat_cnv_sel)
+rownames(DCIS_copykat_cnv_sel_all) <- rownames(DCIS_copykat_cnv_sel)
+colnames(DCIS_copykat_cnv_sel_all)[1] <- 'sample'
 
+##clinical info
+dcis_clinical_0924 <- read.csv('./dcis_project_tissuetype_0916.csv', header = T)
+tissue_type_upd1 <- dcis_clinical_0924[, c(1,2,5)] 
+tissue_type_upd1$sample <- sub('_pc1|_24hTis|_cryo|_24htis', '', tissue_type_upd1$sample)
 
+DCIS_copykat_cnv_sel_all_sample_tissue <- right_join(tissue_type_upd1, DCIS_copykat_cnv_sel_all, by = 'sample')
+rownames(DCIS_copykat_cnv_sel_all_sample_tissue) <- rownames(DCIS_copykat_cnv_sel_all)
+DCIS_copykat_cnv_sel_all_sample_tissue <- DCIS_copykat_cnv_sel_all_sample_tissue %>% filter(!ER == 'neg') %>% filter(!tissue_upd_0924 == 'mucinous')
+DCIS_copykat_cnv_sel_all_sample_tissue <- arrange(DCIS_copykat_cnv_sel_all_sample_tissue, match(DCIS_copykat_cnv_sel_all_sample_tissue$tissue_upd_0924, c("DCIS_yes", "synch_yes", "IDC_yes")))
 
+sample_record <- DCIS_copykat_cnv_sel_all_sample_tissue$sample
+tissue_record <- DCIS_copykat_cnv_sel_all_sample_tissue$tissue_upd_0924
 
+DCIS_copykat_cnv_sel_all_sample_tissue$sample <- NULL
+DCIS_copykat_cnv_sel_all_sample_tissue$ER <- NULL
+DCIS_copykat_cnv_sel_all_sample_tissue$tissue_upd_0924 <- NULL
+DCIS_copykat_cnv_sel_all_scale1 <- DCIS_copykat_cnv_sel_all_sample_tissue
 
+corresponding_values <- paste0("p", seq_along(unique(sample_record)))
+sample_record1 <- data.frame(x = corresponding_values[match(sample_record, unique(sample_record))])
 
+chrom <- DCIS_copykat_cnv$chrom
+hb <- HeatmapAnnotation(chr=factor(chrom,levels = c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23)),show_legend = F,annotation_name_side="left",which="column",
+                        col=list(chr = c("1"="grey","2"="black","3"="grey","4"="black","5"="grey","6"="black","7"="grey","8"="black","9"="grey","10"="black","11"="grey",
+                                         "12"="black","13"="grey","14"="black","15"="grey","16"="black","17"="grey","18"="black","19"="grey","20"="black","21"="grey","22"="black","23"="grey")))
 
+ht <- ComplexHeatmap::Heatmap(DCIS_copykat_cnv_sel_all_scale1, cluster_rows = F ,cluster_columns=F, border="black", raster_device = "png", show_row_names = F, show_column_names = F, row_gap = unit(0,"mm"), 
+                              top_annotation = hb, column_gap = unit(0,"mm"), column_split = DCIS_copykat_cnv$chrom, row_order = rownames(DCIS_copykat_cnv_sel_all_scale1), row_split = factor(sample_record1$x, 
+                              levels = paste0('p', c(1:length(unique(sample_record1$x))))), col = circlize::colorRamp2(c(min(DCIS_copykat_cnv_sel_all_scale1), -0.4, 0, 0.55, max(DCIS_copykat_cnv_sel_all_scale1)), 
+                              c("#26456EFF", "#1C5E9DFF", "white", "#B3101BFF", "#9C0824FF")), cluster_row_slices = F)
 
+row_ht <- Heatmap(tissue_record, name = "type", width = 1)
 
+p_com <- cowplot::plot_grid(grid::grid.grabExpr(ComplexHeatmap::draw(ht + row_ht)))
 
-
+print(p_com)
 
 
 ###--------Supplementary figure 1b--------###
+hbca_dcis_ibc.integrated.b1.b2.clean.cellanno <- readRDS('./hbca_dcis_ibc.integrated.b1.b2.clean.cellanno.rds')
+hbca_dcis_ibc.integrated.b1.b2.clean.cellanno$celltype_com <- hbca_dcis_ibc.integrated.b1.b2.clean.cellanno$celltype_upd1
+hbca_dcis_ibc.integrated.b1.b2.clean.cellanno$celltype_com <- factor(hbca_dcis_ibc.integrated.b1.b2.clean.cellanno$celltype_com, 
+                                                                     levels = c("Basal", "lumhr", "lumsec",  "Bcell", "NK_T", "Myeloid", "Fibroblast", "Vascular", "Lymphatic", "PeriVas"))
 
-DCIS_copykat_cnv_bcmdcis01 <- read.table("/volumes/USR2/rumwei/Analysis/DCIS/CopyKat_res/CopyKat_DCIS_SC_NegCon/BCMDCIS01T_copykat_CNA_results.txt", header = T)
+hbca_dcis_ibc.integrated.b1.b2.clean.cellanno$tumor_normal <- ifelse(rownames(hbca_dcis_ibc.integrated.b1.b2.clean.cellanno@meta.data) %in% rownames(tumor_obj_rm_meta_data), 
+                                                                     'aneuploid', 'diploid')
 
-DCIS_copykat_cnv_sel_all <- read.csv('/volumes/USR1/siyuan/DCIS_siyuan/scRNA-seq/final_round/tumor/CNV_heatmap/DCIS_copykat_cnv_sel_all.csv', header = T)
-sample_record <- read.csv('/volumes/USR1/siyuan/DCIS_siyuan/scRNA-seq/final_round/tumor/CNV_heatmap/sample_record.csv', header = T, row.names = 1)
+hbca_dcis_ibc.integrated.all.meta <- data.frame(sample = hbca_dcis_ibc.integrated.b1.b2.clean.cellanno$orig.ident, proj_ori = hbca_dcis_ibc.integrated.b1.b2.clean.cellanno$project,
+                                                celltype = hbca_dcis_ibc.integrated.b1.b2.clean.cellanno$celltype_com, ploidy = hbca_dcis_ibc.integrated.b1.b2.clean.cellanno$tumor_normal)
+
+
+hbca_dcis_ibc.integrated.all.meta.tumor.freq <- data.frame(table(hbca_dcis_ibc.integrated.all.meta$sample, hbca_dcis_ibc.integrated.all.meta$ploidy))
+hbca_dcis_ibc.integrated.all.meta.tumor.tis <- merge(hbca_dcis_ibc.integrated.all.meta.tumor.freq, dcis_clinical_0924, by.x = 'Var1', by.y = 'sample', all.x = T)
+hbca_dcis_ibc.integrated.all.meta.tumor.tis.simp <- hbca_dcis_ibc.integrated.all.meta.tumor.tis[, c(1,2,3,4,7)]
+
+hbca_dcis_ibc.integrated.all.meta.tumor.tis.simp <- hbca_dcis_ibc.integrated.all.meta.tumor.tis.simp %>% filter(!ER == 'neg') %>% filter(!tissue_upd_0924 == 'mucinous')
+
+hbca_dcis_ibc.integrated.all.meta.tumor.tis.simp$Proportion <- hbca_dcis_ibc.integrated.all.meta.tumor.tis.simp$Freq / ave(hbca_dcis_ibc.integrated.all.meta.tumor.tis.simp$Freq, hbca_dcis_ibc.integrated.all.meta.tumor.tis.simp$Var1, FUN = sum)
+hbca_dcis_ibc.integrated.all.meta.tumor.tis.simp$tissue <- factor(hbca_dcis_ibc.integrated.all.meta.tumor.tis.simp$tissue_upd_0924, levels = c("Normal", "DCIS_no", "DCIS_yes", "synch_no", "synch_yes", "IDC_no", "IDC_yes"))
+
+hbca_dcis_ibc.integrated.all.meta.tumor.tis.simp <- hbca_dcis_ibc.integrated.all.meta.tumor.tis.simp %>%
+  arrange(tissue, desc(Var2), desc(Proportion))
+
+ggplot(hbca_dcis_ibc.integrated.all.meta.tumor.tis.simp, aes(fill=Var2, y=Proportion, x=Var1)) + 
+  geom_bar(stat="identity") + scale_x_discrete(limits = unique(hbca_dcis_ibc.integrated.all.meta.tumor.tis.simp$Var1)) + theme_bw()+
+  theme(axis.text.x=element_text(angle=50,hjust=1,vjust=1),axis.text.y = element_text(size=12)) + scale_fill_manual(values = c('#DC863BFF', '#a4ceb7'))
+
+
+###--------Supplementary figure 1c--------###
+DCIS_copykat_cnv_bcmdcis01 <- read.table("./BCMDCIS01T_copykat_CNA_results.txt", header = T)
+
+DCIS_copykat_cnv_sel_all <- read.csv('./DCIS_copykat_cnv_sel_all.csv', header = T)
+sample_record <- read.csv('./sample_record.csv', header = T, row.names = 1)
 
 colnames(DCIS_copykat_cnv_sel_all) <- c('cell', paste(DCIS_copykat_cnv_bcmdcis01$chrom, DCIS_copykat_cnv_bcmdcis01$chrompos, sep = '_'))
 
 DCIS_copykat_cnv_sel_all_sample <- cbind(sample_record, DCIS_copykat_cnv_sel_all)
 colnames(DCIS_copykat_cnv_sel_all_sample)[1] <- 'sample'
 
-#tissue_type_upd1 <- tissue_type_upd[, c(1:3)]
-tissue_type_upd1 <- dcis_clinical_0924[, c(1,2,5)] #using updated classfication
+tissue_type_upd1 <- dcis_clinical_0924[, c(1,2,5)]
 tissue_type_upd1$sample <- sub('_pc1|_24hTis|_cryo|_24htis', '', tissue_type_upd1$sample)
 
 DCIS_copykat_cnv_sel_all_sample_tissue <- merge(tissue_type_upd1, DCIS_copykat_cnv_sel_all_sample, by = 'sample')
 
 colnames(DCIS_copykat_cnv_sel_all_sample_tissue)[c(5:12171)] <- paste('chr', colnames(DCIS_copykat_cnv_sel_all_sample_tissue)[c(5:12171)], sep = '_')
 
-#only select ER+ samples
-#DCIS_copykat_cnv_sel_all_sample_tissue <- DCIS_copykat_cnv_sel_all_sample_tissue %>% filter(!ER == 'neg') %>% filter(!tissue_upd_2024_final == 'mucinous')
 DCIS_copykat_cnv_sel_all_sample_tissue <- DCIS_copykat_cnv_sel_all_sample_tissue %>% filter(!ER == 'neg') %>% filter(!tissue_upd_0924 == 'mucinous')
 
-#binarize the matrix and caculate the freq (thuis is for all and per tissue; cutoff for amp 0.2; loss -0.2)
-##this is for both gain and loss; so run twice but need to change parameters!!!!!!!
+##this is for both gain and loss; so run twice but need to change parameters
 {
   DCIS_copykat_cnv_sel_all_sample_tissue_bin <- DCIS_copykat_cnv_sel_all_sample_tissue %>%
     pivot_longer(cols = starts_with("chr"), names_to = "chr", values_to = "value") %>%
     #mutate(value = ifelse(value < -0.2, 1, 0)) %>%  ##loss
     mutate(value = ifelse(value > 0.2, 1, 0)) %>% #gain
-    ##group_by(sample, tissue_upd_2024_final, chr) %>%
     group_by(sample, tissue_upd_0924, chr) %>%
     summarize(count = sum(value > 0)) %>%
     ungroup()
@@ -60,15 +124,12 @@ DCIS_copykat_cnv_sel_all_sample_tissue <- DCIS_copykat_cnv_sel_all_sample_tissue
   
   #frequency
   DCIS_copykat_cnv_sel_all_sample_tissue_bin_sel_freq <- DCIS_copykat_cnv_sel_all_sample_tissue_bin_sel %>%
-    #group_by(tissue_upd_2024_final, chr) %>%
     group_by(tissue_upd_0924, chr) %>%
     summarize(frequency = mean(count > 1))
   
-  ######only for loss events!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  ######only for loss events
   DCIS_copykat_cnv_sel_all_sample_tissue_bin_sel_freq$frequency <- -1 * DCIS_copykat_cnv_sel_all_sample_tissue_bin_sel_freq$frequency
   
-  ##choose tissues
-  #tissue_copykat_cnv_sel_all_sample_tissue_bin_sel_freq <- DCIS_copykat_cnv_sel_all_sample_tissue_bin_sel_freq %>% filter(tissue_upd_2024_final == 'DCIS_yes')
   tissue_copykat_cnv_sel_all_sample_tissue_bin_sel_freq <- DCIS_copykat_cnv_sel_all_sample_tissue_bin_sel_freq
   tissue_copykat_cnv_sel_all_sample_tissue_bin_sel_freq <- tissue_copykat_cnv_sel_all_sample_tissue_bin_sel_freq %>% mutate(split_strings = strsplit(chr, "_"),
                                                                                                                             chromosome = sapply(split_strings, function(x) x[[2]]))
@@ -76,13 +137,8 @@ DCIS_copykat_cnv_sel_all_sample_tissue <- DCIS_copykat_cnv_sel_all_sample_tissue
     mutate(chr = factor(chr, levels = paste('chr', paste(DCIS_copykat_cnv_bcmdcis01$chrom, DCIS_copykat_cnv_bcmdcis01$chrompos, sep = '_'), sep = '_'))) %>%
     arrange(chr)
   tissue_copykat_cnv_sel_all_sample_tissue_bin_sel_freq_ord$chromosome <- factor(tissue_copykat_cnv_sel_all_sample_tissue_bin_sel_freq_ord$chromosome, levels = unique(DCIS_copykat_cnv_bcmdcis01$chrom))
-  # ggplot(tissue_copykat_cnv_sel_all_sample_tissue_bin_sel_freq_ord, aes(x = chr, y = frequency, group = tissue_upd_2024_final)) + geom_line(color = 'blue') + geom_area(aes(y=frequency), fill='blue', alpha=1, position = "identity") + 
-  #   facet_grid(.~ chromosome, scales = "free_x", space = "free_x") + theme_bw() + theme(strip.text = element_text(size=10), panel.spacing = unit(-.5, 'pt'), plot.margin = unit(c(0, 0, 0, 0), "cm")) +
-  #   scale_y_continuous(expand = c(0, 0)) + rremove('x.text') + rremove('x.ticks') + rremove('xlab') + rremove('grid')
-  
 }
 
-####frequency plot for all
 #record gain
 all_copykat_cnv_sel_all_sample_all_bin_sel_freq_amp <- tissue_copykat_cnv_sel_all_sample_tissue_bin_sel_freq_ord
 colnames(all_copykat_cnv_sel_all_sample_all_bin_sel_freq_amp) <- c('all', 'pos', 'amp', 'str', 'chr')
@@ -94,22 +150,13 @@ all_copykat_gain_loss <- merge(all_copykat_cnv_sel_all_sample_all_bin_sel_freq_a
 all_copykat_gain_loss_pos <- all_copykat_gain_loss[,c(1,2,3,5,6)]
 colnames(all_copykat_gain_loss_pos) <- c('pos', 'tissue', 'gain', 'chr', 'loss')
 
-ggplot(all_copykat_gain_loss_pos) + geom_line(aes(pos, gain, group = tissue, color = tissue),linewidth = 0.7) + geom_line(aes(pos, loss, group = tissue, color = tissue),linewidth = 0.7) + 
-  facet_grid(.~ chr, scales = "free_x", space = "free_x") + theme_bw() + theme(strip.text = element_text(size=10), panel.spacing = unit(-.5, 'pt'), plot.margin = unit(c(0, 0, 0, 0), "cm")) +
-  rremove('x.text') + rremove('x.ticks') + rremove('xlab') + rremove('grid') + scale_color_manual(values = c("#F9B90AFF", "#F24C3DFF"))
-
 all_copykat_gain_loss_pos1 <- all_copykat_gain_loss_pos
 all_copykat_gain_loss_pos1 <- all_copykat_gain_loss_pos1 %>% separate(pos, into = c("chr", "chr_num", "chr_pos"), sep = "_")
 all_copykat_gain_loss_pos1$chr_num <- factor(all_copykat_gain_loss_pos1$chr_num, unique(DCIS_copykat_cnv_bcmdcis01$chrom))
 all_copykat_gain_loss_pos1$chr_pos <- as.numeric(all_copykat_gain_loss_pos1$chr_pos)
 
-ggplot(all_copykat_gain_loss_pos1) + geom_line(aes(chr_pos, gain, group = tissue, color = tissue),linewidth = 0.7) + geom_line(aes(chr_pos, loss, group = tissue, color = tissue),linewidth = 0.7) + 
-  facet_grid(.~ chr_num, scales = "free_x", space = "free_x") + theme_bw() + theme(strip.text = element_text(size=10), panel.spacing = unit(-.5, 'pt'), plot.margin = unit(c(0, 0, 0, 0), "cm")) +
-  rremove('x.text') + rremove('x.ticks') + rremove('xlab') + rremove('grid') + scale_color_manual(values = c("#F9B90AFF", "#F24C3DFF", "blue"))
-
-
 ## Add breast cancer genes to freq plot ##
-bc_gene_pos <- read.table('/volumes/USR1/siyuan/reference/Nick_BC_genes.sorted.bed', sep='\t') %>% 
+bc_gene_pos <- read.table('./BC_genes.sorted.bed', sep='\t') %>% 
   set_colnames(c('chr', 'start', 'end', 'gene')) %>% set_rownames(.$gene)
 
 {
@@ -122,20 +169,29 @@ bc_gene_pos <- read.table('/volumes/USR1/siyuan/reference/Nick_BC_genes.sorted.b
   bc_gene_select_df$chr <- gsub("chr", "", bc_gene_select_df$chr)
   bc_gene_select_df$chr <- factor(bc_gene_select_df$chr, unique(DCIS_copykat_cnv_bcmdcis01$chrom))
   bc_gene_select_df$chr_num <- bc_gene_select_df$chr
-  }
-
-#bc_gene_select_df$po <- paste(bc_gene_select_df$chr_s, bc_gene_select_df$mid, sep = '_')
-
-# ggplot(all_copykat_gain_loss_pos1) + geom_line(aes(chr_pos, gain, group = tissue, color = tissue),linewidth = 0.85) + geom_line(aes(chr_pos, loss, group = tissue, color = tissue),linewidth = 0.85) + 
-#   #ggrepel::geom_text_repel(data=bc_gene_select_df, aes(x=mid, y=0.8, label = bc_gene), size = 3) +
-#   geom_vline(data=bc_gene_select_df, aes(xintercept=mid),  linetype='dotted', size=.5) +
-#   facet_grid(.~ chr_num, scales = "free_x", space = "free_x") + theme_bw() + theme(strip.text = element_text(size=10), panel.spacing = unit(-.5, 'pt'), plot.margin = unit(c(0, 0, 0, 0), "cm")) +
-#   rremove('x.text') + rremove('x.ticks') + rremove('xlab') + rremove('grid') + scale_color_manual(values = c("#F9B90AFF", "#F24C3DFF"))
+}
 
 all_copykat_gain_loss_pos1$tissue <- factor(all_copykat_gain_loss_pos1$tissue, levels = c("DCIS_yes",  "synch_yes", "IDC_yes"))
 
-ggplot(all_copykat_gain_loss_pos1) + geom_line(aes(chr_pos, gain, group = tissue, color = tissue),linewidth = 0.75) + geom_line(aes(chr_pos, loss, group = tissue, color = tissue), linewidth = 0.75) + 
-  ggrepel::geom_text_repel(data=bc_gene_select_df, aes(x=mid, y=0.9, label = bc_gene), size = 3) +
-  geom_vline(data=bc_gene_select_df, aes(xintercept=mid),  linetype='dotted', size=.5) +
+ggplot(all_copykat_gain_loss_pos1) + geom_line(aes(chr_pos, gain, group = tissue, color = tissue), linewidth = 0.75) + geom_line(aes(chr_pos, loss, group = tissue, color = tissue), linewidth = 0.75) + 
+  ggrepel::geom_text_repel(data=bc_gene_select_df, aes(x = mid, y = 0.9, label = bc_gene), size = 3) +
+  geom_vline(data = bc_gene_select_df, aes(xintercept = mid),  linetype = 'dotted', size = .5) +
   facet_grid(.~ chr_num, scales = "free_x", space = "free_x") + theme_bw() + theme(strip.text = element_text(size=10), panel.spacing = unit(-.5, 'pt'), plot.margin = unit(c(0, 0, 0, 0), "cm")) +
   rremove('x.text') + rremove('x.ticks') + rremove('xlab') + rremove('grid') + scale_color_manual(values = c("#F9B90AFF", "#41B6E6FF", "#F24C3DFF"))+ ylim(-1, 1)
+
+
+###--------Supplementary figure 1d--------###
+hbca_dcis_ibc.integrated.all.meta <- data.frame(sample = hbca_dcis_ibc.integrated.b1.b2.clean.cellanno$orig.ident, proj_ori = hbca_dcis_ibc.integrated.b1.b2.clean.cellanno$project,
+                                                celltype = hbca_dcis_ibc.integrated.b1.b2.clean.cellanno$celltype_com, ploidy = hbca_dcis_ibc.integrated.b1.b2.clean.cellanno$tumor_normal)
+
+hbca_dcis_ibc.integrated.all.celltype.freq <- data.frame(table(hbca_dcis_ibc.integrated.all.meta$sample, hbca_dcis_ibc.integrated.all.meta$celltype))
+
+hbca_dcis_ibc.integrated.all.celltype.freq.sel <- hbca_dcis_ibc.integrated.all.celltype.freq %>% filter(Var1 %in% unique(hbca_dcis_ibc.integrated.all.meta.tumor.tis.simp$Var1))
+
+ggplot(hbca_dcis_ibc.integrated.all.celltype.freq.sel %>% filter(!Var2 %in% c('lumhr', 'lumsec')), aes(fill=Var2, y=Freq, x=Var1)) + 
+  geom_bar(position = 'fill', stat="identity") + scale_x_discrete(limits = unique(hbca_dcis_ibc.integrated.all.meta.tumor.tis.simp$Var1)) + theme_bw()+
+  theme(axis.text.x=element_text(angle=50,hjust=1,vjust=1),axis.text.y = element_text(size=12)) + scale_fill_manual(values = c(celltype_col[1], celltype_col[4:10]))
+
+
+
+
