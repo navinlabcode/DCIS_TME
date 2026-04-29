e3bdcccf48792c08192eff658a20daa757633abb
Siyuan <415110769@qq.com>
2026-04-29 11:20:33 -0500

    Update mainfigure_manuscript.R

--- Figures/Figure7/mainfigure_manuscript.R
+++ Figures/Figure7/mainfigure_manuscript.R
@@ -1,19 +1,22 @@
 # fig7a-cell state correlation-------------------------------------------------------------------
+
 all_cell_count_24 <- read.csv('./filtered/all_cell_merged_anno.csv')
 
-## the file should look like this: BCMHBCA83L_3h,lumsec,Lumsec_PTN,AAACGAAAGTGTAGTA-1_1_1_1
+## the input file (all_cell_count_24) should look like this: 
+## sample, celltype, cellstate, unique_id
+##BCMHBCA83L_3h,lumsec,Lumsec_PTN,AAACGAAAGTGTAGTA-1_1_1_1
 
 all_cell_count_24$unique_id <- paste(all_cell_count_24$sample, sub('-.*', '', all_cell_count_24$cell_id), sep = '_')
 
 # get tumor cell ID info
+tumor_obj_rm_meta_data <- read.csv('./tumor/tumor_obj_rm_sub_diet_meta.csv', row.names = 1)
+
+## tumor_obj_rm_meta_data file should contain rownames like: AAACCCATCGGAATTC-1_1_3 and orig.ident: BCMDCIS01T_pc1
+
 tumor_id <- data.frame(id = rownames(tumor_obj_rm_meta_data), sample = tumor_obj_rm_meta_data$orig.ident)
 tumor_id$unique_id <- paste(tumor_id$sample, sub('-.*', '', tumor_id$id), sep = '_')
 
 all_cell_count_24$celltype1 <- ifelse(all_cell_count_24$unique_id %in% tumor_id$unique_id, 'tumor', all_cell_count_24$celltype)
-
-##all_cell_count_24 <- read.csv('./filtered/all_cell_merged_anno_upd.csv', row.names = 1)
-
-##all_cellstate_count_24 <- all_cell_count_24[, c(2,1,6,4)]
 
 # only extract tme cells
 all_tme_cell_count_24 <- all_cell_count_24 %>% filter(!celltype1 %in% c('lumhr', 'tumor', 'lumsec', 'Lymphatic'))
